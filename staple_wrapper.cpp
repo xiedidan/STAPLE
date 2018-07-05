bedcbd4af39f1e3f113e308f94fbf7f4187d1240
Xie Di <xiedidan@yeah.net>
2018-07-05 21:04:24 +0800

    dev commit - compiled wrapper, needs debugging

--- staple_wrapper.cpp
+++ staple_wrapper.cpp
@@ -3,48 +3,87 @@
 #include <boost/python.hpp>
 #include <boost/python/numpy.hpp>
 
-//namespace p = boost::python;
-//namespace np = boost::python::numpy;
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace boost::python;
+
+namespace p = boost::python;
+namespace np = boost::python::numpy;
 
 class Staple {
     public:
     Staple() {
-        //Py_Initialize();
-        //np::initialize();
+        Py_Initialize();
+        np::initialize();
 
-        // tracker = new STAPLE_TRACKER();
+        tracker = new STAPLE_TRACKER();
     };
 
     ~Staple() {
-        //delete(tracker);
+        delete(tracker);
     };
 
-    void init(/*np::ndarray image, np::ndarray bbox*/) {
-        // TODO : convert image from ndarray to cv::Mat
+    void init(np::ndarray image, np::ndarray bbox) {
+        // convert image from ndarray to cv::Mat
+        cv::Mat im = cv::Mat(
+            image.shape(0),
+            image.shape(1),
+            CV_8UC1,
+            image.get_data()
+        );
 
-        // TODO : convert bbox from ndarray to cv::Rect_<float>
+		// convert bbox from ndarray to cv::Rect_<float>
+		float* bboxData = (float*)bbox.get_data();
+        cv::Rect_<float> region(
+            float(bboxData[2] - bboxData[0] / 2.0),
+            float(bboxData[3] - bboxData[1] / 2.0),
+            float(bboxData[2] - bboxData[0]),
+            float(bboxData[3] - bboxData[1])
+        );
 
-        //tracker.tracker_staple_initialize(im, region);
-        //tracker.tracker_staple_train(im, true);
+        tracker->tracker_staple_initialize(im, region);
+        tracker->tracker_staple_train(im, true);
     };
 
-    // np::ndarray update(np::ndarray image) {
-        //cv::Rect region = tracker.tracker_staple_update(im);
-        //tracker.tracker_staple_train(im, false);
+    np::ndarray update(np::ndarray image) {
+        // convert image from ndarray to cv::Mat
+        cv::Mat im = cv::Mat(
+            image.shape(0),
+            image.shape(1),
+            CV_8UC1,
+            image.get_data()
+        );
 
-        // TODO : convert bbox to ndarray
+        cv::Rect_<float> region = tracker->tracker_staple_update(im);
+        tracker->tracker_staple_train(im, false);
 
-        //return bbox;
-    //};
+        // convert region to ndarray bbox
+        float bboxArray[] = {
+            region.tl().x,
+            region.tl().y,
+            region.br().x,
+            region.br().y
+        };
+        
+        np::ndarray bbox = np::from_data(
+			bboxArray,
+			np::dtype::get_builtin<float>(),
+			p::make_tuple(4),
+			p::make_tuple(sizeof(float)),
+			p::object()
+		);
+
+        return bbox;
+    };
 
     private:
-    //STAPLE_TRACKER* tracker;
+    STAPLE_TRACKER* tracker;
 };
-/*
+
 BOOST_PYTHON_MODULE(StapleWrapper) {
     class_<Staple>("Staple", init<>())
-        .def("init", $Staple::init)
-        .def("update", $Staple::update)
+        .def("init", &Staple::init)
+        .def("update", &Staple::update)
     ;
 }
-*/
